005676251246f5c6cc15a447b69018cf5bd8c728
Bharat Rajagopalan <bharat@establishonline.io>
2022-09-02 19:10:33 +0530

    Fixed SEO component error

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import { SEO } from "../components/seo";
 import Form from "../components/appointment-form";
 import Card from "../components/card";
 import ArrowButtonA from "../components/arrow-button-a";
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Layout from "../components/layout";
 
+import Layout from "../components/layout";
 import Hero from "../components/home-hero";
 import Services from "../components/home-services";
 import About from "../components/home-about";
 import Appointment from "../components/home-appointment";
+import { SEO } from "../components/seo";
 
 const IndexPage = () => {
 	return (
