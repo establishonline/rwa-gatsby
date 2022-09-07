ca9ad83c3e5daf632fcb070d8b258f137c0d193f
Bharat <bharat@establishonline.io>
2022-09-07 15:41:28 +0000

    cookie test

--- src/pages/dev-2.jsx
+++ src/pages/dev-2.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 import useCookie from "../hooks/useCookie";
 
 const DevTwo = () => {
-    const [cookie] = useCookie("username", false);
+    const [cookie] = useCookie("username", "abc");
 
 	return (
 		<Layout>
