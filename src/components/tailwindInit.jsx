f1bc87804dcef64912d99735938a0ece989d97cd
Bharat Rajagopalan <bharat@establishonline.io>
2022-09-08 13:30:05 +0530

    Fixed border radius not working

---
+++ src/components/tailwindInit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+// Used to init tailwindCSS that won't work otherwise
+
+export default TailwindInit = () => {
+	return <div className='bg-blue rounded-br-10xl rounded-bl-10xl'></div>;
+};
